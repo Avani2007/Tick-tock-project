57648954d856a2bdfa3864dd76b2f09c4cba61d7
Avani2007 <69082362+Avani2007@users.noreply.github.com>
2020-12-10 16:01:52 +0530

    Add files via upload

---
+++ PRO C42 - tick tock/sketch.js
@@ -0,0 +1,58 @@
+var hrAngle, mnAngle, scAngle;
+
+var hr, mn, sc;
+
+function setup() {
+  createCanvas(800, 400);
+  //createSprite(400, 200, 50, 50);
+  angleMode(DEGREES);
+
+}
+
+function draw() {
+  background(255, 255, 255);
+  translate(200,200)
+  rotate(-90)
+  hr = hour();
+  mn = minute();
+  sc = second();
+
+  hrAngle = map(hr % 12, 0, 12, 0, 360)
+  mnAngle = map(mn, 0, 60, 0, 360)
+  scAngle = map(sc, 0, 60, 0, 360)
+  noFill();
+  //hour
+  push();
+  rotate(hrAngle)
+  stroke(0, 0, 255)
+  strokeWeight(7)
+  line(0, 0, 75, 0)
+  pop()
+  
+  stroke(0,0,255)
+  arc(0,0,260,260,0,hrAngle)
+
+ // min
+ push();
+ rotate(mnAngle)
+ stroke(0, 255,0)
+ strokeWeight(7)
+ line(0, 0, 75, 0)
+ pop()
+ 
+ stroke(0,255,0)
+ arc(0,0,245,245,0,mnAngle)
+
+ //sec
+ push();
+ rotate(scAngle)
+ stroke(255,0,0)
+ strokeWeight(7)
+ line(0, 0, 75, 0)
+ pop()
+ 
+ stroke(255,0,0)
+ arc(0,0,230,230,0,scAngle)
+
+  drawSprites();
+}
